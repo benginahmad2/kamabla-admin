eff5e1d013d700e17b8f426a5324f586f70a3bf6
Talenthubrecruitment <hendrysakdiyah@gmail.com>
2024-10-20 21:58:46 +0900

    fix:RPC

--- src/components/section/WalletProvider.tsx
+++ src/components/section/WalletProvider.tsx
@@ -5,16 +5,17 @@ import {
 } from "@solana/wallet-adapter-react";
 
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { clusterApiUrl } from "@solana/web3.js";
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+// import { clusterApiUrl } from "@solana/web3.js";
+// import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import {
 
   SolflareWalletAdapter,
 } from "@solana/wallet-adapter-wallets";
 import "@solana/wallet-adapter-react-ui/styles.css";
 
-const network = WalletAdapterNetwork.Mainnet; // Use MainnetBeta or Testnet as needed
-const endpoint = clusterApiUrl(network);
+// const network = WalletAdapterNetwork.Mainnet; // Use MainnetBeta or Testnet as needed
+// const endpoint = clusterApiUrl(network);
+const endpoint = "https://solana-mainnet.g.alchemy.com/v2/HPBJFOauHz3LPGOp0JIOHe1FnPuk5q2q";
 
 interface WalletProviderProps {
   children: ReactNode;
